5767827819670ef25e9b3e8fbd0806223989d6e5
Brandon Lee <leebr48@gmail.com>
2023-07-17 10:44:35 +0200

    Changes to plot DMerc, and some cleaning

--- eRootProject/NTSSstuff/plotOthers.py
+++ eRootProject/NTSSstuff/plotOthers.py
@@ -1,8 +1,10 @@
-# This script plots the results from epseff.txt and magwell.txt, which were produced using NEO and STELLOPT for *ALL SURFACES POSSIBLE* in the configuration.
+# This script plots epseff and DMerc for the initial and final configurations. These are calculated on *ALL SURFACES POSSIBLE* for each configuration.
 
 #Inputs
 rhoMin = 0
 rhoMax = 0.85
+DMercYMin = -0.01
+DMercYMax = 0.01
 fileExt = 'png'
 
 ########################################################################################
@@ -11,21 +13,30 @@ fileExt = 'png'
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+from scipy.io import netcdf_file
 sys.path.append('/home/blee/src/stelloptPlusSfincs/src')
 from dataProc import createVMECGrids
 
 # Load data
-epseff = np.loadtxt('epseff.txt')
+epseff_opt = np.loadtxt('epseff_opt.txt')
 epseff_w7xhm = np.loadtxt('epseff_w7xhm.txt')
-magwell = np.loadtxt('magwell.txt')
-magwell_w7xhm = np.loadtxt('magwell_w7xhm.txt')
+
+wout_opt = netcdf_file('../DKESstuff/wout_Lee_1.nc', mode='r', mmap=False)
+wout_init = netcdf_file('../initialConfigs/wout_W7X_REACTOR_FULL_woptim_forSfincs_correctPres.nc', mode='r', mmap=False)
+ns_opt = wout_opt.variables['ns'][()]
+ns_init = wout_init.variables['ns'][()]
+if ns_opt == ns_init:
+    ns = ns_opt
+else:
+    raise IOError('"ns" for the initial and optimized configurations differ - something is wrong.')
+DMerc_opt = wout_opt.variables['DMerc'][()]
+DMerc_init = wout_init.variables['DMerc'][()]
 
 # Process data
-ns = len(epseff) + 1 # The magnetic axis is ignored by NEO, so we must include the + 1
 _, fulls = createVMECGrids(ns)
-sgrid = fulls[1:] # Again, ignore the magnetic axis
+sgrid = fulls[1:] # Ignore the magnetic axis because eps_eff cannot be evaluated there by NEO.
 rhogrid = np.sqrt(sgrid)
-data = np.column_stack((rhogrid, epseff, epseff_w7xhm, magwell[1:], magwell_w7xhm[1:])) # Need to ignore axis for magnetic well
+data = np.column_stack((rhogrid, epseff_opt, epseff_w7xhm, DMerc_opt[1:], DMerc_init[1:])) # Need to ignore axis for magnetic well
 filteredData = data[(data[:, 0] >= rhoMin) & (data[:, 0] <= rhoMax)]
 
 # Plot data
@@ -36,17 +47,18 @@ plt.yticks(np.arange(0,np.ceil(np.max(filteredData[:,1]))+1, 1))
 plt.xlim(xmin=0)
 plt.xlabel(r'$\rho$')
 plt.ylabel(r'$\epsilon_\mathrm{eff}$ (%)')
-plt.legend(['Opt. Config.','Init. Config.'])
+plt.legend(['Opt. Config.', 'W7-X High-Mirror'])
 plt.savefig('epseff'+'.'+fileExt, bbox_inches='tight', dpi=400)
 
 plt.figure()
 plt.plot(filteredData[:,0], filteredData[:,3])
 plt.plot(filteredData[:,0], filteredData[:,4])
 plt.xlim(xmin=0)
+plt.ylim(ymin=DMercYMin, ymax=DMercYMax)
 plt.xlabel(r'$\rho$')
-plt.ylabel(r'Magnetic well')
-plt.legend(['Opt. Config.','Init. Config.'])
+plt.ylabel(r'$D_\mathrm{Merc}$')
+plt.legend(['Opt. Config.', 'Init. Config.'])
 
-plt.savefig('magwell'+'.'+fileExt, bbox_inches='tight', dpi=400)
+plt.savefig('DMerc'+'.'+fileExt, bbox_inches='tight', dpi=400)
 
 plt.show()
