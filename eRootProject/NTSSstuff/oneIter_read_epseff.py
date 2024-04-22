4d73cdc4a90a9a45369c2b525a565465bce54cf1
Brandon Lee <leebr48@gmail.com>
2024-04-22 23:59:45 +0200

    Redid epseff profile storage. Still untested though!

---
+++ eRootProject/NTSSstuff/oneIter_read_epseff.py
@@ -0,0 +1,11 @@
+dataFile = 'stellopt.W7X_REACTOR_woptim_forSfincs'
+fname = 'lee1_epseff'
+ftype = 'txt'
+
+import numpy as np
+
+data = np.loadtxt(dataFile, skiprows=7, max_rows=127, usecols=(2))
+
+epseff = data ** (2/3)
+
+np.savetxt(fname + '.' + ftype, epseff)
